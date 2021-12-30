6c8c1926e26b34417a6effe9fffbb10191bece23
nation430 <tonyenation430@gmail.com>
2021-12-30 21:58:04 +0100

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", navToggel);
+const navigationBar = document.querySelector(".main-header");
+function navToggel() {
+  if (window.scrollY > 170) {
+    navigationBar.classList.add("fixedNavbar");
+  } else {
+    navigationBar.classList.remove("fixedNavbar");
+  }
+}
+
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+
+button.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
