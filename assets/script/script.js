704e3d6b73fe71d74705d268444354461e7830c8
T.NATION <tonyenation430@gmail.com>
2022-01-15 14:05:11 +0100

    added other pages

--- assets/script/script.js
+++ assets/script/script.js
@@ -17,3 +17,39 @@ button.addEventListener('click', () => {
 });
 
 
+      //  Social Media Icons Hover Effect
+    tippy('#facebook',{
+      content:'Facebook',
+      animation:'fade',
+      delay:[200,200]
+    });
+    tippy('#instagram',{
+      content:'Instagram',
+      animation:'fade',
+      delay:[200,200]
+    });
+     tippy('#whatsapp',{
+      content:'Whatsapp',
+      animation:'fade',
+      delay:[200,200]
+    });
+    tippy('#twitter',{
+      content:'Twitter',
+      animation:'fade',
+      delay:[200,200]
+    });
+
+    tippy('#linkedin',{
+      content:'Linkedin',
+      animation:'fade',
+      placement: 'top',
+      delay:[200,200]
+    });
+     tippy('#snapchat',{
+      content:'Snapchat',
+      animation:'fade',
+      placement: 'top',
+      delay:[200,200]
+    });
+ 
+
